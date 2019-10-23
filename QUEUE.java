a0778abc91e4904969305fe19bae31e582084a5f
WIRASATRIA PUTRA <56837285+Wirasatria@users.noreply.github.com>
2019-10-23 22:28:12 +0700

    Add files via upload

---
+++ QUEUE.java
@@ -0,0 +1,57 @@
+package queue_tugas;
+
+
+public class QUEUE {
+    
+    String data[];
+    int tail;
+    int antrian;
+
+    public QUEUE(int x){
+        data = new String[x];
+        tail = -1;
+        antrian = 0;
+    }
+
+    public void masuk(String nomor) {
+        if (tail < data.length) {
+            antrian++;
+
+            data[++tail] = nomor;
+            System.out.println(" nomor antrian :" + antrian );
+            System.out.println("Selamat datang " + nomor);
+
+        } else {
+            System.out.println("Sabar harap mengantri");
+        }
+    }
+
+    public String keluar(){
+        if(tail > 0){
+            antrian--;
+            String temp = data[0];
+            --tail;
+
+            for (int i = 0; i < data.length-1 ; i++) {
+                data[i] = data[i+1];
+
+            }
+            return temp;
+        }
+        else{
+            System.out.println("SILAHKAN TUNGGU SEBENTAR :");
+        }
+
+        return "";
+    }
+
+    public void print(){
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("nomor antrian " + (i+1) );
+            System.out.println("Selamat datang = " + data[i]);
+        }
+    }
+
+}
+
+
---
+++ run_queue.java
@@ -0,0 +1,45 @@
+package queue_tugas;
+
+
+public class run_queue {
+
+    public static void main(String[] args) {
+        QUEUE antri = new QUEUE(5);
+
+        antri.masuk("Pelanggan 1 :");
+        antri.masuk("Pelanggan 2 :");
+        antri.masuk("Pelanggan 3 :");
+        System.out.println("=====================================");
+
+        System.out.println(antri.keluar()+"Selamat Menikmati");
+
+        System.out.println();
+        System.out.println();
+        antri.print();
+        System.out.println("========================");
+
+        antri.masuk("PELANGGAN 1");
+
+        System.out.println("============================");
+        antri.print();
+        System.out.println("============================");
+
+        antri.masuk("PELANGGAN 2");
+        System.out.println("====================================   ");
+        antri.print();
+        System.out.println("========================================");
+
+        System.out.println(antri.keluar() + "Selamat Menikmati");
+
+        System.out.println();
+        antri.masuk("Pelanggan 4");
+        System.out.println();
+        antri.print();
+
+
+
+    }
+}
+
+    
+
